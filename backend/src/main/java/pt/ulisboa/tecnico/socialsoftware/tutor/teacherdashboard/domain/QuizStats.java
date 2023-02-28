bfd5c1f1ac0798c5931c1502d4ac55d814608804
Martim Abreu <martim.abreu@tecnico.ulisboa.pt>
2023-02-28 15:50:53 +0000

    feat: Created class QuizStats with attributes, closes #4

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/QuizStats.java
@@ -0,0 +1,27 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain;
+
+import pt.ulisboa.tecnico.socialsoftware.tutor.execution.domain.CourseExecution;
+import pt.ulisboa.tecnico.socialsoftware.tutor.impexp.domain.DomainEntity;
+import pt.ulisboa.tecnico.socialsoftware.tutor.impexp.domain.Visitor;
+
+public class QuizStats implements DomainEntity {
+    private Integer numQuizzes;
+    private Integer uniqueQuizzesSolved;
+    private float averageQuizzesSolved;
+
+    public QuizStats(){
+    }
+
+    public void accept(Visitor visitor) {
+        // Only used for XML generation
+    }
+    @Override
+    public String toString() {
+        return "QuizStats{" +
+                "numQuizzes=" + numQuizzes +
+                ", uniqueQuizzesSolved=" + uniqueQuizzesSolved +
+                ", averaqeQuizzesSolved=" + averageQuizzesSolved +
+                '}';
+    }
+
+}

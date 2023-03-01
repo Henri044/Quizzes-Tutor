b123f3b92e6e2bd0e2e06e02a5644939c9c06dbe
Lourenço Completo <lourenco.completo@tecnico.ulisboa.pt>
2023-03-01 16:45:04 +0000

    fix(domain): bug fixes

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/QuizStats.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/QuizStats.java
@@ -4,6 +4,10 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.execution.domain.CourseExecution;
 import pt.ulisboa.tecnico.socialsoftware.tutor.impexp.domain.DomainEntity;
 import pt.ulisboa.tecnico.socialsoftware.tutor.impexp.domain.Visitor;
 import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.QuizService;
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Student;
+import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.domain.Quiz;
+import pt.ulisboa.tecnico.socialsoftware.tutor.answer.domain.QuizAnswer;
+import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.TeacherDashboard;
 
 import javax.persistence.*;
 import java.util.Set;
@@ -32,7 +36,7 @@ public class QuizStats implements DomainEntity {
 
     public void setCourseExecution(CourseExecution courseExecution) { this.courseExecution = courseExecution; }
 
-    public TeacherDashboard getTeacherDashboard() { return TeacherDashboard; }
+    public TeacherDashboard getTeacherDashboard() { return teacherDashboard; }
 
     public void setTeacherDashboard(TeacherDashboard teacherDashboard) { this.teacherDashboard = teacherDashboard; }
 
@@ -77,7 +81,7 @@ public class QuizStats implements DomainEntity {
 
         this.setUniqueQuizzesSolved(quizzes.size());
 
-        this.setAverageQuizzesSolved() = getUniqueQuizzesSolved() / students.size();
+        this.setAverageQuizzesSolved(getUniqueQuizzesSolved() / students.size());
     }
 
     public void accept(Visitor visitor) {

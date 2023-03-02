5a2a0ce49c0fdb6215a5a9c89f4e072852ec9536
Daniel Costa <daniel.p.costa@tecnico.ulisboa.pt>
2023-03-02 23:11:51 +0000

    tests: tests toString and sets/gets , close #29 and #31

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/CreateStudentStatsTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/CreateStudentStatsTest.groovy
@@ -25,7 +25,7 @@ class CreateStudentStatsTest extends SpockTest {
     def setup() {
         createExternalCourseAndExecution()
 
-        teacher = new Teacher("JoÃ£o",false)
+        teacher = new Teacher(USER_1_NAME,false)
         UserRepository.save(teacher)
 
         teacherDashboard = new TeacherDashboard(externalCourseExecution, teacher)
@@ -37,6 +37,62 @@ class CreateStudentStatsTest extends SpockTest {
         StudentStatsRepository.save(studentStats)
     }
 
+	def "testing setNumStudents and getNumStudents"() {
+		given: "a teacherDashboard and a teacher"
+
+		when: "create a StudentStats and set the number of students"
+		def studentsStats = createStudentStats()
+		studentsStats.setNumStudents(4)
+
+		then: "check if the number of students has been set correctly"
+		def result = studentStatsRepository.findAll().get(0)
+		studentsStats.getNumStudents() == result.getNumStudents()
+		studentsStats.getNumStudents() == 4
+
+	} 
+
+	def "testing setNumMore75CorrectQuestions and getNumMore75CorrectQuestions"() {
+		given : "a teacherDashboard and a teacher"
+
+		when: "create a StudentStats and set the number of students with 75% more questions correctly"
+		def studentsStats = createStudentStats()
+		studentsStats.setNumMore75CorrectQuestions(4)
+
+		then: "check if the variable numMore75CorrectQuestions has been set correctly"
+		def result = studentStatsRepository.findAll().get(0)
+		studentsStats.getNumMore75CorrectQuestions() == result.getNumMore75CorrectQuestions()
+		studentsStats.getNumMore75CorrectQuestions() == 4
+	} 
+
+	def "testing setNumAtLeast3Quizzes and getNumAtLeast3Quizzes"() {
+		given : "a teacherDashboard and a teacher"
+
+		when: "create a StudentStats and set the number of students with at least 3 quizzes answended"
+		def studentsStats = createStudentStats()
+		studentsStats.setNumAtLeast3Quizzes(4)
+
+		then: "check if the variable numAtLeast3Quizzes has been set correctly"
+		def result = studentStatsRepository.findAll().get(0)
+		studentsStats.getNumAtLeast3Quizzes() == result.getNumAtLeast3Quizzes()
+		studentsStats.getNumAtLeast3Quizzes() == 4
+	}
+
+
+	def "testing toString"(){
+		given: "a teacherDashboard and a teacher"
+
+		when: "create a StudentStats and set the variables of the students"
+		def studentsStats = createStudentStats()
+		studentsStats.setNumMore75CorrectQuestions(4)
+		studentsStats.setNumAtLeast3Quizzes(5)
+		studentsStats.setNumStudents(6)
+		def auxString = "StudentStats{" + "id=" + studentsStats.getId() +", numStudents=6, numMore75CorrectQuestions=4, numAtLeast3Quizzes=5}"
+
+		then: "check if toString match the predicted string"
+		def result = studentStatsRepository.findAll().get(0)
+		studentsStats.toString().equals(result.toString()) == true
+		studentsStats.toString().equals(auxString) == true
+	}
 
     def "create an empty studentStats"() {
 
@@ -55,7 +111,6 @@ class CreateStudentStatsTest extends SpockTest {
 
     	and:
     	teacherDashboard.getStudentStats().size() == 1
-    	//teacherDashboard.getStudentStats() != null
     	teacherDashboard.getStudentStats().contains(result)
     }
 

85262a3d1df80428dac4bf59c4bf4fdae453b74e
ruipmfs <rui.pmfs@gmail.com>
2023-03-14 22:58:17 +0000

    feat(api): implementation of REST service for method updateTeacherDashboard(), closes #37

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/api/TeacherDashboardController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/api/TeacherDashboardController.java
@@ -5,6 +5,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pt.ulisboa.tecnico.socialsoftware.tutor.auth.domain.AuthUser;
 import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.dto.TeacherDashboardDto;
@@ -27,6 +28,12 @@ public class TeacherDashboardController {
         int teacherId = ((AuthUser) ((Authentication) principal).getPrincipal()).getUser().getId();
 
         return teacherDashboardService.getTeacherDashboard(courseExecutionId, teacherId);
+    }
+
+    @PutMapping("/teachers/dashboards/update-all-teacherdashboards")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void updateAllTeacherDashboards() {
+        this.teacherDashboardService.updateAllTeacherDashboards();
     }
 
 }

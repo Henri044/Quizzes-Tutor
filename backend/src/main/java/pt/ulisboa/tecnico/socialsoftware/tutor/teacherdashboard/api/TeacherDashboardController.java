7c1f913d557cd8ce41f3de1622cb20ad51b76a66
Lourenço Completo <lourenco.completo@tecnico.ulisboa.pt>
2023-03-14 22:00:54 +0000

    feat(api): implemented REST service for removeTeacherDashboard, closes #39

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/api/TeacherDashboardController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/api/TeacherDashboardController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pt.ulisboa.tecnico.socialsoftware.tutor.auth.domain.AuthUser;
@@ -27,6 +28,12 @@ public class TeacherDashboardController {
         int teacherId = ((AuthUser) ((Authentication) principal).getPrincipal()).getUser().getId();
 
         return teacherDashboardService.getTeacherDashboard(courseExecutionId, teacherId);
+    }
+
+    @DeleteMapping("/teachers/dashboards/remove-teacherdashboard")
+    @PreAuthorize("hasRole('ROLE_TEACHER') and hasPermission(#dashboardId, 'DASHBOARD.ACCESS)")
+    public void removeTeacherDashboard(Principal principal, @PathVariable int dashboardId) {
+        teacherDashboardService.removeTeacherDashboard(dashboardId);
     }
 
 }

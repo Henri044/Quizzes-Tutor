f5a0b11b144ee28e013724a24c163d9fb43c3569
Daniel Costa <daniel.p.costa@tecnico.ulisboa.pt>
2023-03-14 23:06:01 +0000

    feat(Dto): Implement StudentStats to TeacherDashboardDto and added the StudentStatsDto class, closes #41

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/TeacherDashboardDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/TeacherDashboardDto.java
@@ -1,10 +1,16 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.dto;
 
+import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.StudentStats;
 import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.TeacherDashboard;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TeacherDashboardDto {
     private Integer id;
     private Integer numberOfStudents;
+
+    private List<StudentStatsDto> studentStats = new ArrayList<>();
 
     public TeacherDashboardDto() {
     }
@@ -13,6 +19,10 @@ public class TeacherDashboardDto {
         this.id = teacherDashboard.getId();
         // For the number of students, we consider only active students
         this.numberOfStudents = teacherDashboard.getCourseExecution().getNumberOfActiveStudents();
+        List<StudentStats> aux_studentStats = teacherDashboard.getStudentStats();
+        for(StudentStats x : aux_studentStats){
+            this.studentStats.add(new StudentStatsDto(x));
+        }
     }
 
     public Integer getId() {
@@ -29,6 +39,14 @@ public class TeacherDashboardDto {
 
     public void setNumberOfStudents(Integer numberOfStudents) {
         this.numberOfStudents = numberOfStudents;
+    }
+
+    public List<StudentStatsDto> getStudentStats() {
+        return studentStats;
+    }
+
+    public void setStudentStats(List<StudentStatsDto> stats) {
+        this.studentStats = stats;
     }
 
     @Override

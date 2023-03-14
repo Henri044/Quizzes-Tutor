622759c265f26b0d4e59f70d2ff8475309829cab
Bernardo Paulo <bernardo.paulo@tecnico.ulisboa.pt>
2023-03-14 19:52:08 +0000

    Implement QuestionStats to TeacherDashboardDto and added the QuestionStatsDto class, closes #36

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/TeacherDashboardDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/TeacherDashboardDto.java
@@ -1,10 +1,17 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.dto;
 
+import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.QuestionStats;
+import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.QuizStats;
 import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.TeacherDashboard;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TeacherDashboardDto {
     private Integer id;
     private Integer numberOfStudents;
+
+    private List<QuestionStatsDto> questionStatsDtoList = new ArrayList<>();
 
     public TeacherDashboardDto() {
     }
@@ -13,6 +20,10 @@ public class TeacherDashboardDto {
         this.id = teacherDashboard.getId();
         // For the number of students, we consider only active students
         this.numberOfStudents = teacherDashboard.getCourseExecution().getNumberOfActiveStudents();
+        List<QuestionStats> auxQuestionStats = teacherDashboard.getQuestionStats();
+        for(QuestionStats x: auxQuestionStats) {
+            this.questionStatsDtoList.add(new QuestionStatsDto(x));
+        }
     }
 
     public Integer getId() {
@@ -29,6 +40,14 @@ public class TeacherDashboardDto {
 
     public void setNumberOfStudents(Integer numberOfStudents) {
         this.numberOfStudents = numberOfStudents;
+    }
+
+    public List<QuestionStatsDto> getQuestionStats() {
+        return questionStatsDtoList;
+    }
+
+    public void setQuestionStats(List<QuestionStatsDto> stats) {
+        this.questionStatsDtoList = stats;
     }
 
     @Override
